76380ee540e32a8678b0fe68a938d3003edc6d9e
romainmontagnon <montagnon.romain@gmail.com>
2021-04-08 17:42:07 +0200

    mise en place des premieres structure de codes, schedule a faire sous github

---
+++ models/sauce.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    userId          : {type: String, required: true},
+    name            : {type: String, required: true},
+    manufacturer    : {type: String, required: true},
+    description     : {type: String, required: true},
+    mainPepper      : {type: String, required: true},
+    imageUrl        : {type: String},
+    heat            : {type: Number, required: true},
+    likes           : {type: Number, required: true},
+    usersLiked      : {type: String},
+    usersDisLiked   : {type: String}
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
